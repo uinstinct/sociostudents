6ec9a7d478c2524be93611b1981f3455607dcf0d
uinstinct <uinstinct@outlook.com>
2020-03-10 21:57:49 +0800

    now displaying dynamic data with some dummy entries

--- sociostudents/views.py
+++ sociostudents/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
 def homepage(request):
     return render(request,'homepage.html')
+
+
+def all_students(request):
+    students = Student.objects.all()
+    return render(request, 'students/all_students.html', {'students': students})
