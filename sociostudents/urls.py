e65cfc559c8c557d847448f810afa2399c6174ef
uinstinct <uinstinct@outlook.com>
2020-03-10 22:40:44 +0800

    Profile page but looks ugly

--- sociostudents/urls.py
+++ sociostudents/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.homepage,name='homepage'),
-    path('students', views.all_students, name='students')
+    path('students', views.all_students, name='students'),
+    path('students/<str:username>', views.list_student,name='list_student'),
 ]
--- sociostudents/views.py
+++ sociostudents/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Student
 
 # Create your views here.
@@ -9,3 +9,8 @@ def homepage(request):
 def all_students(request):
     students = Student.objects.all()
     return render(request, 'students/all_students.html', {'students': students})
+
+def list_student(request, username):
+    student =  get_object_or_404(Student, username=username)
+    return render(request, 'students/list_student.html', {'student':student})
+    
